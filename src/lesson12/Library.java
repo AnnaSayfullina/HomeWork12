8c6f8dbd827340616ee450194fde3160b24ccfe0
anna <anna@sayf.su>
2023-01-11 12:27:19 +0300

    задание Сложный уровень

---
+++ src/lesson12/Library.java
@@ -0,0 +1,42 @@
+package lesson12;
+
+public class Library {
+    private Book[] allBooks = new Book[5];
+    private int sizeArray;
+    public Library(int sizeArray){
+        allBooks = new Book[sizeArray];
+        this.sizeArray = sizeArray;
+    }
+    public void addBookToArray(Book nameBook) {
+        for (int i = 0; i < allBooks.length; i++){
+            if (allBooks[i] == null) {
+                allBooks[i] = nameBook;
+                return;
+            }
+        }
+    }
+    public void printBook (Book[]  nameBook) {
+        for (int i = 0; i < nameBook.length; i++) {
+            System.out.printf("%s: %s: %d%n", nameBook[i].getAuthor(), nameBook[i].getName(), nameBook[i].getYear());
+        }
+    }
+    public void printInformationNameBook(Book[] nameBook, String nameOfBook ){
+        for (int i = 0; i<nameBook.length; i++){
+            boolean equals = nameBook[i].getName().equals(nameOfBook);
+            if (equals) {
+                System.out.printf("Автор книга %s - %s. Книга была издана в %d%n", nameBook[i].getName(), nameBook[i].getAuthor(), nameBook[i].getYear());
+            }
+        }
+    }
+    public void changePublicationYear(Book[] nameBook, String nameOfBook, int newYear){
+        for (int i = 0; i<nameBook.length; i++){
+            boolean equals = nameBook[i].getName().equals(nameOfBook);
+            if (equals) {
+                nameBook[i].setYear(newYear);
+                System.out.printf("Год публикации книги %s изменен на %d%n", nameBook[i].getName(), nameBook[i].getYear());
+            }
+        }
+    }
+
+
+}
--- src/lesson12/Main.java
+++ src/lesson12/Main.java
@@ -29,9 +29,30 @@ package lesson12;
  *     каждая новая книга должна успешно укладываться в массив.
  *     2. Напечатать в консоль все книги из массива в следующем формате:
  *         “Stephen King: The Stand: 1978”
+
+ *         ## Сложный уровень
+  * 1. Создать класс Library, который будет иметь своим полем массив из прошлого уровня.
+ *     Учесть, что доступ к нему должен быть только из Library (private поле), а сеттеры и геттеры отсутствуют.
+ *     Работать с массивом можно только через внутренние нестатические методы класса Library.
+ * 2. Конструктор класса должен получать в качестве параметра размер массива и инициализировать в себе поле
+ *      массива тем размером, что пришел параметром.
+ * 3. Перенести методы из прошлого уровня в класс Library, сделать их нестатическими.
+ * 4. Создать новые нестатические методы в классе Library:
+ *     1. Напечатать информацию о книге по ее названию, которое приходит в метод в качестве параметра.
+ *         Формат: “The Stand by Stephen King was published in 1978”
+ *     2. Изменить год публикации книги по ее названию.
+ *         В метод подаются два параметра, а именно: название и новый год публикации.
+ *         Нужно найти книгу и изменить ее год публикации.
+ * - **Критерии оценки**
+ *     1. Класс Library создан корректно.
+ *     2. Массив корректно перенесен в виде поля в класс Library.
+ *     3. Конструктор класса Library написан корректно.
+ *     4. Корректно перенесены методы, модификатор static удален.
+ *     5. Объявлены новые методы.
+ *     6. Методы корректно справляются со своей задачей.
  */
 public class Main {
-    static Book[] allBooks = new Book[5];
+    public static Book[] allBooks = new Book[5];
     public static void addBookToArray(Book nameBook) {
         for (int i = 0; i < allBooks.length; i++){
             if (allBooks[i] == null) {
@@ -71,5 +92,16 @@ public class Main {
         addBookToArray(hobbit);
         addBookToArray(queenOfSpades);
         printBook(allBooks);
+        System.out.println();
+
+        System.out.println("Сложный уровень");
+        Library libraries= new Library(6);
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Book warAndPeace = new Book("Война и мир", levTolstoy, 2019);
+        libraries.addBookToArray(warAndPeace);
+        libraries.printBook(allBooks);
+        libraries.printInformationNameBook(allBooks, "Гарри Поттер");
+        libraries.changePublicationYear(allBooks,"Пиковая дама", 2023);
     }
+
 }
